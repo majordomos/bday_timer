7f253bdecc2ca521b0b361e8990fdce52e106a7e
Alexey Mitsenko <alex.micenko@gmail.com>
2021-03-25 21:53:47 +0300

    finish

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const bDay = "11 Feb 2022";
+const daysElement = document.getElementById('days');
+const hoursElement = document.getElementById('hours');
+const minutesElement = document.getElementById('minutes');
+const secondsElement = document.getElementById('seconds');
 
 function tick() {
     const bDayDate = new Date(bDay);
     const currDate = new Date();
     const diff = bDayDate - currDate;
-    const seconds = Math.floor(diff / 1000);
-    const minutes = Math.floor(seconds / 60) % 60;
-    const hours = Math.floor(seconds / 3600) % 24;
-    const days = Math.floor(seconds / 3600 / 24);
+
+    const totalSeconds = Math.floor(diff / 1000);
+    const seconds = Math.floor(totalSeconds % 60);
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const days = Math.floor(totalSeconds / 3600 / 24);
 
 
+    secondsElement.innerHTML = formatTime(seconds);
+    minutesElement.innerHTML = formatTime(minutes);
+    hoursElement.innerHTML = formatTime(hours);
+    daysElement.innerHTML = days;
 
-    console.log(
-        `days - ${days}, 
-        hours - ${hours}, 
-        minutes - ${minutes},
-        seconds - ${seconds}`);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
 }
 
 tick();
